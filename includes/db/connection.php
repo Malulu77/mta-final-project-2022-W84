5f7a4c229b20b406f96da9d2ed4114b86b48ebe1
Amit Malul <68725582+Malulu77@users.noreply.github.com>
2022-07-23 23:54:29 +0300

    Create connection.php

---
+++ includes/db/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+// to use db locally replace localhost with 172.24.2.2
+$server_name = "localhost";
+$db_user_name = "isamitml_user";
+$db_password = "iG_W7XXnV!8U";
+$database_name = "isamitml_db2";
+
+//create connection
+$conn = mysqli_connect($server_name, $db_user_name, $db_password, $database_name);
+
+//check the connection
+if ($conn->connect_error){
+    die("Connection failed: ".$conn->connect_error);
+}
